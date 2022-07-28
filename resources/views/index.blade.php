876e30d53a7a7cb4df06609708e647f5359ba9cc
Brian Galang <bryangalangoniel@gmail.com>
2022-07-29 02:38:11 +0800

    laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Reservoir | Landing</title>
+  @vite('resources/css/app.css')
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="bg-slate-100">
+  <main>
+    <div class="big-wrapper content">
+
+      <header>
+        <div class="container">
+          <div class="logo">
+            <img class="flex-none w-12 h-12" src="assets/img/index-icon.png" alt="index-icon">
+            <h3 class="font-sans hover:text-gray-600">RESERVOIR</h3>
+          </div>
+        </div>
+      </header>
+
+      <div class="showcase-area">
+        <div class="container">
+          <div class="left">
+            <div class="big-title">
+              <h1 class="font-sans text-5xl">Welcome to</h1>
+              <h1 class="font-sans text-5xl">Reservoir!</h1>
+            </div>
+            <p class="text-zinc-500 font-sans text-lg leading-9 max-w-xl my-7 mr-0 mb-10 regal-blue">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div class="cta">
+              <a href="#" class="btn bg-sky-400 hover:bg-sky-500">I'm a Vendor</a>
+              <a href="#" class="btn bg-sky-400 hover:bg-sky-500">I'm a Customer</a>
+            </div>
+          </div>
+          <div class="right">
+            <img src="assets/img/shape.png" alt="shape" class="shape" />
+          </div>
+        </div>
+      </div>
+  </main>
+  <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+</body>
+
+</html>
